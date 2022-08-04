9679a279ce2ad4a36533da806bfaa2fb865c323c
Josie <nagy.iozefina@yahoo.com>
2022-08-04 18:23:42 +0200

    Add helper function for data fetching; Switch to functional components
    Add more spacing rules

--- src/Movies.js
+++ src/Movies.js
@@ -1,41 +1,47 @@
-import React, { Component } from "react";
-import { movies } from "./services/fakeMovieService";
+import React, { useEffect, useState } from "react";
+import { getMovies } from "./services/fakeMovieService";
 
-// console.log(movies, 'movies are in Movies component');
+function Movies () {
+  const [data, setData] = useState([]);
 
-class Movies extends Component {
-
-  handleDelete = () => console.log()
-
-  render() {
-    return (
-      <>
-        {!movies.length && <h2>There are no movies in the database</h2>}
-        {movies.length === 1 ? <h2>Showing {movies.length} movie in the database.</h2> :
-          <h2>Showing {movies.length} movies in the database.</h2>}
-        <table>
-          <tbody>
-          <MovieList />
-          </tbody>
-        </table>
-      </>
-    )
-  }
-}
-
-
-function MovieList() {
-  const listItems = movies.map(movie => <tr>
-    <th key={movie['_id']}>{movie.title}</th>
-    <th key={movie.genre['_id']}>{movie.genre.name}</th>
-    <th key={movie['_id']}>{movie.numberInStock}</th>
-    <th key={movie['_id']}>{movie.dailyRentalRate}</th>
-    {/*<button type="button" class="btn btn-danger" onClick={this.handleDelete}>Delete</button>*/}
-  </tr>);
+  useEffect(() => {
+      setData(getMovies())
+    }, []
+  );
 
   return (
     <>
-      {listItems}
+      {<h2>Showing {data.length} movie{data.length === 1 ? '' : 's'} in the database.</h2>}
+      {!data.length && <h2>There are no movies in the database</h2>}
+      <table>
+        <tbody>
+        <MovieList movieData={data} />
+        </tbody>
+      </table>
+    </>
+  )
+
+}
+
+
+function MovieList (props) {
+
+  const handleDelete = () => console.log('handleDelete triggered');
+
+  return (
+    <>
+      {props.movieData.map(movie => {
+        return (
+          <tr>
+            <th key={movie['_id']}>{movie.title}</th>
+            <th key={movie.genre['_id']}>{movie.genre.name}</th>
+            <th key={movie['_id']}>{movie.numberInStock}</th>
+            <th key={movie['_id']}>{movie.dailyRentalRate}</th>
+            <button type="button" className="btn btn-danger" onClick={handleDelete}>Delete</button>
+          </tr>
+        );
+      })
+      }
     </>
   )
 }
