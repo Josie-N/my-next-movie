98b06f560ba961b5c33412a24e7f774ff9c5300d
Josie <nagy.iozefina@yahoo.com>
2022-08-05 19:46:40 +0200

    Fix console errors: same key issue and rendering logic error. Add h1.
    Add style-components for next phase.

--- src/Movies.js
+++ src/Movies.js
@@ -9,22 +9,40 @@ function Movies () {
     }, []
   );
 
+  // Don't forget: Add loading state
+  // Suggestion: add pagination
+  // Suggestion: use a real api instead
+
+  const visuallyHidden = {
+    position: 'absolute',
+    left: -10000,
+    top: 'auto',
+    width: 1,
+    height: 1,
+    overflow: 'hidden'
+  }
+
+  if (data.length < 1) return <h2>There are no movies in the database.</h2>
+
+
   return (
     <>
-      {<h2>Showing {data.length} movie{data.length === 1 ? '' : 's'} in the database.</h2>}
-      {!data.length && <h2>There are no movies in the database</h2>}
-      <table className="table table-striped">
-        <thead>
-        <tr>
-          <th scope="col"><strong>Title</strong></th>
-          <th scope="col">Genre</th>
-          <th scope="col">Stock</th>
-          <th scope="col">Rate</th>
-          <th scope="col"></th>
-        </tr>
-        </thead>
-        <MovieList movieData={data} />
-      </table>
+      <h1 style={visuallyHidden}>Vidly movie database</h1>
+      <h2>Showing {data.length} movie{data.length > 1 ? 's' : ''} in the database.</h2>
+      <main>
+        <table className="table table-striped">
+          <thead>
+          <tr>
+            <th scope="col"><strong>Title</strong></th>
+            <th scope="col">Genre</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Rate</th>
+            <th scope="col"></th>
+          </tr>
+          </thead>
+          <MovieList movieData={data} />
+        </table>
+      </main>
     </>
   )
 }
@@ -38,11 +56,11 @@ function MovieList (props) {
     <tbody>
     {props.movieData.map(movie => {
       return (
-        <tr>
-          <td key={movie['_id']}>{movie.title}</td>
-          <td key={movie.genre['_id']}>{movie.genre.name}</td>
-          <td key={movie['_id']}>{movie.numberInStock}</td>
-          <td key={movie['_id']}>{movie.dailyRentalRate}</td>
+        <tr key={movie['_id']}>
+          <td>{movie.title}</td>
+          <td>{movie.genre.name}</td>
+          <td>{movie.numberInStock}</td>
+          <td>{movie.dailyRentalRate}</td>
           <button type="button" className="btn btn-danger" onClick={handleDelete}>Delete</button>
         </tr>
       );
