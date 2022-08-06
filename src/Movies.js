b030a120a93498ac57051de0179bf0aa146bd6b5
Josie <nagy.iozefina@yahoo.com>
2022-08-06 16:17:09 +0200

    Refactor/ Improve code quality

--- src/Movies.js
+++ src/Movies.js
@@ -22,22 +22,24 @@ function Movies () {
     overflow: 'hidden'
   }
 
-  if (data.length < 1) return <h2>There are no movies in the database.</h2>
+  const movieCount = data.length;
+
+  if (movieCount < 1) return <h2>There are no movies in the database.</h2>
 
 
   return (
     <>
       <h1 style={visuallyHidden}>Vidly movie database</h1>
-      <h2>Showing {data.length} movie{data.length > 1 ? 's' : ''} in the database.</h2>
+      <h2>Showing {movieCount} movie{movieCount > 1 ? 's' : ''} in the database.</h2>
       <main>
         <table className="table table-striped">
           <thead>
           <tr>
-            <th scope="col"><strong>Title</strong></th>
+            <th scope="col">Title</th>
             <th scope="col">Genre</th>
             <th scope="col">Stock</th>
             <th scope="col">Rate</th>
-            <th scope="col"></th>
+            <th scope="col" />
           </tr>
           </thead>
           <MovieList movieData={data} />
@@ -50,18 +52,20 @@ function Movies () {
 
 function MovieList (props) {
 
-  const handleDelete = () => console.log('handleDelete triggered');
+  const handleDelete = (movie) => console.log(movie);
 
   return (
     <tbody>
-    {props.movieData.map(movie => {
+    {movieData.map(movie => {
       return (
         <tr key={movie['_id']}>
           <td>{movie.title}</td>
           <td>{movie.genre.name}</td>
           <td>{movie.numberInStock}</td>
           <td>{movie.dailyRentalRate}</td>
-          <button type="button" className="btn btn-danger" onClick={handleDelete}>Delete</button>
+          <td>
+            <button type="button" className="btn btn-danger btn-sm" onClick={() => handleDelete(movie)}>Delete</button>
+          </td>
         </tr>
       );
     })
