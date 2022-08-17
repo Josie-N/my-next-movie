a39260f87191686a1e0787e52959ca7d8bfe3e03
Josie <nagy.iozefina@yahoo.com>
2022-08-17 18:08:15 +0200

    Add styling and conditional rendering for the current page number
    Map through all movies available to create pagination. 
    Determine how many page numbers will be visible to the user

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import _ from 'lodash';
+import classNames from "classnames";
 
 function Pagination ({ totalPagesAvailable }) {
+
+  const [currentPage, setCurrentPage] = useState(1);
+
   if (totalPagesAvailable === 1) return null;
 
   // _.range() creates an array of numbers
   // Result: 50 (integer) is converted to: [1, 2, 3, ... , 50] (array with no 0 index)
-  const pages = _.range(1, totalPagesAvailable + 1);
+  let pages = _.range(1, totalPagesAvailable + 1);
 
+  if (totalPagesAvailable > 3) {
+    pages = pages.slice(0, 3)
+  }
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
 
   return (
     <>
-      <nav role="navigation" aria-label="Pagination Navigation">
-        {pages.map(pageNumber =>
-          <ul key={pageNumber} className="pagination">
-            {/*<li onClick={onPageChange} className={pageStatusClass}>*/}
-            <li>
-              <a className="page-link" aria-label={`Go to Page ${pageNumber}`}>{pageNumber}</a>
-            </li>
-          </ul>
-        )}
+      <nav
+        role="navigation"
+        aria-label="Pagination Navigation"
+        className="d-flex align-items-baseline"
+      >
+        {pages.map(pageNumber => {
+          const pageIsActive = pageNumber === currentPage;
+          const activePageNumberStyling = classNames('page-item', { 'active': pageIsActive });
+
+          return (
+            <ul key={pageNumber} className="pagination">
+              <li className={activePageNumberStyling}>
+                <a aria-current={pageIsActive}
+                   aria-label={`Go to Page ${pageNumber}`}
+                   className="page-link"
+                   onClick={() => handlePageChange(pageNumber)}
+                >
+                  {pageNumber}
+                </a>
+              </li>
+            </ul>
+          )
+        })}
       </nav>
     </>
   );
