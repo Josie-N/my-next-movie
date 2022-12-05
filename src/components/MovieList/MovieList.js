1eea626caf4dac2435089685e50bf8f0fcd38c82
Josie <nagy.iozefina@yahoo.com>
2022-12-05 15:41:49 +0100

    Refactor: extract movie card list into it's own separate component

---
+++ src/components/MovieList/MovieList.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from "../../views/Homepage/Homepage.module.css";
+import PropTypes from "prop-types";
+
+import PaginationNumberScreenReader from "../PaginationNumberScreenReader/PaginationNumberScreenReader";
+import MovieCard from "../MovieCard/MovieCard";
+import PaginationNumber from "../PaginationNumber/PaginationNumber";
+
+const MovieList = ({ movies, numberOfMoviesPerPage }) => {
+  return (
+    <div className={styles.movieCardContainer} aria-live="polite">
+      {movies && movies.map((movie, index) => {
+
+        // Move all of these calcs to calculatePagination function
+        const quotient = index / numberOfMoviesPerPage;
+        const pageNumber = Math.floor(quotient) + 1;
+
+        const divisionRemainder = index % numberOfMoviesPerPage;
+
+        // Output: 4, 9, 14, 19, ...
+        const indexOfLastCardOnEachPage = (numberOfMoviesPerPage - 1);
+        const showPageNumberOnLastCard = divisionRemainder === indexOfLastCardOnEachPage;
+
+        // Output: 0, 5, 10, 15, ...
+        const indexOfFirstCardOnEachPage = (numberOfMoviesPerPage - 5);
+        const showPageNumberOnFirstCard = divisionRemainder === indexOfFirstCardOnEachPage
+
+        return (
+          <>
+            <PaginationNumberScreenReader
+              pageNumber={pageNumber}
+              showPageNumberOnFirstCard={showPageNumberOnFirstCard}
+            />
+            <MovieCard key={movie._id}
+                       movie={movie}
+            />
+            <PaginationNumber
+              pageNumber={pageNumber}
+              showPageNumberOnFirstCard={showPageNumberOnFirstCard}
+              showPageNumberOnLastCard={showPageNumberOnLastCard}
+            />
+          </>
+        );
+      })}
+    </div>
+  );
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.array.isRequired,
+  numberOfMoviesPerPage: PropTypes.number.isRequired,
+};
+
+export default MovieList;
--- src/views/Homepage/Homepage.js
+++ src/views/Homepage/Homepage.js
@@ -3,15 +3,10 @@ import PropTypes from "prop-types";
 
 import styles from "./Homepage.module.css";
 import helperStyles from "../../assets/stylesheets/helper.module.css";
-
-import MovieCard from "../../components/MovieCard/MovieCard";
 import Spinner from "../../components/generic/Spinner/Spinner";
 import Button from "../../components/generic/Button/Button";
-
-import PaginationNumber from "../../components/PaginationNumber/PaginationNumber";
-import PaginationNumberScreenReader from "../../components/PaginationNumberScreenReader/PaginationNumberScreenReader";
+import MovieList from "../../components/MovieList/MovieList";
 import { WatchlistSidebar } from "../../components/WatchlistSidebar/WatchlistSidebar";
-
 
 const Homepage = ({ isLoading, movies, totalPageCount, currentPage, numberOfMoviesPerPage, loadMoreMovies }) => {
   return (
@@ -21,61 +16,32 @@ const Homepage = ({ isLoading, movies, totalPageCount, currentPage, numberOfMovi
           <h2 className={helperStyles.visuallyHidden}>
             Browse all movies available:
           </h2>
-          <div>
             {/*To do: move into its own MovieCardPage component*/}
-            {isLoading ?
-              <div className={styles.movieCardContainerSkeleton}>
-                <Spinner />
-              </div>
-              :
-              <div className={styles.movieCardContainer} aria-live="polite">
-                {movies && movies.map((movie, index) => {
+          {isLoading ?
+            <div className={styles.movieCardContainerSkeleton}>
+              <Spinner />
+            </div>
+            :
+            <div>
+              <MovieList movies={movies} numberOfMoviesPerPage={numberOfMoviesPerPage} />
                   const quotient = index / numberOfMoviesPerPage;
                   const pageNumber = Math.floor(quotient) + 1;
-
-                  const divisionRemainder = index % numberOfMoviesPerPage;
-
-                  // Output: 4, 9, 14, 19, ...
-                  const indexOfLastCardOnEachPage = (numberOfMoviesPerPage - 1);
-                  const showPageNumberOnLastCard = divisionRemainder === indexOfLastCardOnEachPage;
-
-                  // Output: 0, 5, 10, 15, ...
-                  const indexOfFirstCardOnEachPage = (numberOfMoviesPerPage - 5);
-                  const showPageNumberOnFirstCard = divisionRemainder === indexOfFirstCardOnEachPage
-
-                  return (
-                    <>
-                      <PaginationNumberScreenReader
-                        pageNumber={pageNumber}
-                        showPageNumberOnFirstCard={showPageNumberOnFirstCard}
-                      />
-                      <MovieCard key={movie._id}
-                                 movie={movie}
-                      />
-                      <PaginationNumber
-                        pageNumber={pageNumber}
-                        showPageNumberOnFirstCard={showPageNumberOnFirstCard}
-                        showPageNumberOnLastCard={showPageNumberOnLastCard}
-                      />
-                    </>
-                  );
-                })}
+              <div className={styles.showMoreMovies}>
+                {
+                  (currentPage === totalPageCount) ?
+                    <h4>No more movies to load.</h4>
+                    :
+                    <Button ariaLabel="Show more movies" hasIcon icon="👇" type="button"
+                            handleButtonClick={loadMoreMovies}>
+                      <span>SHOW MORE</span>
+                    </Button>
+                }
               </div>
-            }
-            <div className={styles.showMoreMovies}>
-              {
-                (currentPage === totalPageCount) ?
-                  <h4>No more movies to load.</h4>
-                  :
-                  <Button ariaLabel="Show more movies" hasIcon icon="👇" type="button"
-                          handleButtonClick={loadMoreMovies}>
-                    <span>SHOW MORE</span>
-                  </Button>
-              }
             </div>
-          </div>
+          }
           {isLoading ?
-            <Spinner /> :
+            <Spinner />
+            :
             <WatchlistSidebar />
           }
         </main>
