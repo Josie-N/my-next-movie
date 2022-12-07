559a2d3d87750db0ac58fb6707612022958b38ff
Josie <nagy.iozefina@yahoo.com>
2022-12-07 16:20:22 +0100

    Chore: Add TypeScript

--- src/pages/MoviesPage/MovieCard/utils/helper.js
+++ src/pages/MoviesPage/MovieCard/utils/helper.js
@@ -1,8 +1,8 @@
-export const getImdbRatingInteger = (movieRating) => {
+export const getImdbRatingInteger = (movieRating: number): number => {
   return Math.round(movieRating * 10);
 }
 
 // Removes the word 'The' from the beginning of movie titles
-export const getMovieTitle = (movieName) => {
+export const getMovieTitle = (movieName: string): string => {
   return movieName.replace(/The /gm, '');
 }
--- src/pages/MoviesPage/MovieList/utils/calculatePagination.js
+++ src/pages/MoviesPage/MovieList/utils/calculatePagination.js
@@ -1,20 +1,21 @@
 // Adds a number (integer) that increases by 1 after every X amount of movies are listed
-export const getPageNumber = (movieCardIndex, numberOfMoviesPerPage) => {
+export const getPageNumber = (movieCardIndex: number, numberOfMoviesPerPage: number): number => {
+  console.log(typeof movieCardIndex)
   const quotient = movieCardIndex / numberOfMoviesPerPage;
   const pageNumber = Math.floor(quotient) + 1;
 
   return pageNumber;
 }
 
-export const getPageFirstCard = (index, numberOfMoviesPerPage) => {
+export const getPageFirstCard = (movieCardIndex: number, numberOfMoviesPerPage: number): boolean => {
   // Output: 0, 5, 10, 15, ...
   const indexOfFirstCardOnEachPage = (numberOfMoviesPerPage - 5);
-  const showPageNumberOnFirstCard = (index % numberOfMoviesPerPage) === indexOfFirstCardOnEachPage;
+  const showPageNumberOnFirstCard = (movieCardIndex % numberOfMoviesPerPage) === indexOfFirstCardOnEachPage;
 
   return showPageNumberOnFirstCard;
 }
 
-export const getPageLastCard = (movieCardIndex, numberOfMoviesPerPage) => {
+export const getPageLastCard = (movieCardIndex: number, numberOfMoviesPerPage: number): boolean => {
   // Output: 4, 9, 14, 19, ...
   const indexOfLastCardOnEachPage = (numberOfMoviesPerPage - 1);
   const showPageNumberOnLastCard = (movieCardIndex % numberOfMoviesPerPage) === indexOfLastCardOnEachPage;
