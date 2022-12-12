9c3ff385bd92061ac2cde6ce4f7bdc10dc9de54e
Josie <nagy.iozefina@yahoo.com>
2022-12-12 19:04:35 +0100

    Feat: Add api call for movies user added out of the recommended list

--- src/services/api.js
+++ src/services/api.js
@@ -13,6 +13,19 @@ export const getRecommendedMovieList = async (currentPage, numberOfMoviesPerPage
   return json.data;
 }
 
+export const getAdddedMovieList = async (currentPage, numberOfMoviesPerPage) => {
+  const currentPageParam = `page=${currentPage}`;
+  const moviePageLimitParam = `limit=${numberOfMoviesPerPage}`;
+
+  const listType = `list=added`;   // hardcoded, will be replaced with list type
+  const username = `username=sad-panda`;   // hardcoded, will be replaced with generated username
+
+  const url = base + '?' + currentPageParam + '&' + moviePageLimitParam + '&' + listType + '&' + username;
+  const json = await axios.get(url);
+
+  return json.data;
+}
+
 export const postMovietoAddedList = async (id) => {
   const movieId = `movieId=${id}`;
 
