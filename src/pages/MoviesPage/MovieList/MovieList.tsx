cec085c1dd6d7593506e7c334d5be3f53c2e18ca
Josie <nagy.iozefina@yahoo.com>
2023-01-17 13:28:13 +0100

    Chore: Add key to movie list array map

--- src/pages/MoviesPage/MovieList/MovieList.tsx
+++ src/pages/MoviesPage/MovieList/MovieList.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from "./MovieList.module.css";
 
 import {numberOfMoviesPerPage} from "../../../constants/constants";
 import {getPageFirstCard, getPageLastCard, getPageNumber} from "./utils/calculatePagination";
@@ -17,32 +16,28 @@ type Props = {
 
 const MovieList = ({movies, handleMoveToAddedList, handleMoveToRemovedList}: Props) => {
     return (
-        <div className={styles.movieCardContainer} aria-live="polite">
+        <div aria-live="polite">
             {movies.data.map((movie, movieCardIndex) => {
-
                 const pageNumber = getPageNumber(movieCardIndex, numberOfMoviesPerPage);
                 const showPageNumberOnFirstCard = getPageFirstCard(movieCardIndex, numberOfMoviesPerPage);
                 const showPageNumberOnLastCard = getPageLastCard(movieCardIndex, numberOfMoviesPerPage);
 
                 return (
-                    <>
+                    <React.Fragment key={movie._id}>
                         <PaginationNumberScreenReader
                             pageNumber={pageNumber}
                             shouldBeDisplayed={showPageNumberOnFirstCard}
-                            // key={movie._id}
                         />
                         <MovieCard
                             movie={movie}
                             handleMoveToAddedList={handleMoveToAddedList}
                             handleMoveToRemovedList={handleMoveToRemovedList}
-                            // key={movie._id}
                         />
                         <PaginationNumber
                             pageNumber={pageNumber}
                             shouldBeDisplayed={showPageNumberOnLastCard}
-                            // key={movie._id}
                         />
-                    </>
+                    </ React.Fragment>
                 );
             })}
         </div>
